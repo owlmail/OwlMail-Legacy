1304e7d95f6e640255a0342850f1681ad7bfabcb
sachin2dehury <sachindehury2015@gmail.com>
2021-07-31 22:32:19 +0530

    Added ViewModels

---
+++ app/src/main/java/github/sachin2dehury/owlmail/viewmodel/MailItemsViewModel.kt
@@ -0,0 +1,18 @@
+package github.sachin2dehury.owlmail.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import github.sachin2dehury.owlmail.repository.MailRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class MailItemsViewModel @Inject constructor(
+    private val mailRepository: MailRepository
+) : ViewModel() {
+
+//    @ExperimentalPagingApi
+//    fun getParsedMails(conversationId: Int) =
+//        mailRepository.getParsedMails(conversationId).cachedIn(viewModelScope)
+
+    fun getToken() = mailRepository.getToken()
+}
