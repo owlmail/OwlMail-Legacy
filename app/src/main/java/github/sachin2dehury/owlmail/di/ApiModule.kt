5cb8427fb6bb54341e1e5756ac85ecb03c1cbdd6
sachin2dehury <sachindehury2015@gmail.com>
2021-07-31 17:33:08 +0530

    Added Retrofit and Moshi implementation

---
+++ app/src/main/java/github/sachin2dehury/owlmail/di/ApiModule.kt
@@ -0,0 +1,46 @@
+package github.sachin2dehury.owlmail.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import github.sachin2dehury.owlmail.api.BASE_URL
+import github.sachin2dehury.owlmail.api.BasicAuthInterceptor
+import github.sachin2dehury.owlmail.api.MailApi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    @Singleton
+    @Provides
+    fun provideBasicAuthInterceptor() = BasicAuthInterceptor()
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(basicAuthInterceptor: BasicAuthInterceptor) = OkHttpClient.Builder()
+        .addInterceptor(basicAuthInterceptor)
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    @Singleton
+    @Provides
+    fun provideMailApi(
+        moshi: Moshi,
+        okHttpClient: OkHttpClient
+    ): MailApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(okHttpClient)
+        .build()
+        .create(MailApi::class.java)
+}
