84ee6977ac1856f4ef9fe0098fb9ac6efd94c6a6
sachin2dehury <sachindehury2015@gmail.com>
2021-08-04 00:45:15 +0530

    Migrated to gradle kts

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+
+        //Dagger Hilt
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+
+        //Safe Args
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+
+        //Google Services
+        classpath ("com.google.gms:google-services:4.3.8")
+
+        //Firebase Crashlytics
+        classpath ("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
